988a69863b59dc89c31c8f200f5f7f24df3062ce
Vanina Gutierrez <vani2215@gmail.com>
2021-09-21 12:17:15 -0300

    clase 1 de javaScrip

---
+++ javaScrip/js/app.js
@@ -0,0 +1,19 @@
+
+   alert("hola mundo")
+   console.log("estamos en la consola")
+   let nombre = "vani manni";
+   var numero = 10;
+   const  PI=3.1416;
+   let hayClases=true;
+   let esFeriado = true;
+   console.log(numero+25 +" "+nombre)
+    console.log(hayClases)
+    let saludar;
+    let saludo = ("hola como estas?");
+   
+    
+    saludar = saludo + nombre;
+
+let elemento = document.getElementById("principal");
+elemento.innerHTML="<h1 class='text-center bg-info'>"+ saludar + "</h1>";
+document.write("<h1 class='text-center bg-info'>"+ saludar + "</h1>");
